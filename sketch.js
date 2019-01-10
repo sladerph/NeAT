daac93ec3264d4c53f148bc6d1d9b55de059b7f7
Pierre Herduin <herduinpierre@yahoo.com>
2019-01-10 16:46:44 +0100

    animated player

--- sketch.js
+++ sketch.js
@@ -15,41 +15,77 @@ var config = {
   },
   scene: {
     preload: preload,
-    create: create
+    create: create,
+    update: update,
   }
 };
 
 var game = new Phaser.Game(config);
 
 function preload() {
-  // this.load.image('idle', 'Sprites/idle.png');
-  // this.load.image('move_right', 'Sprites/right.png');
-  // this.load.image('move_left', 'Sprites/left.png');
-  // this.load.image('jump_up', 'Sprites/jumping.png');
-  // this.load.image('jump_down', 'Sprites/falling.png');
-  // this.load.image('kick', 'Sprites/shooting.png');
-  this.load.image('dude', 'Sprites/sprite_sheet.png');
+  this.load.image('idle', 'Sprites/idle.png');
+  this.load.image('move_right', 'Sprites/right.png');
+  this.load.image('move_left', 'Sprites/left.png');
+  this.load.image('jump_up', 'Sprites/jumping.png');
+  this.load.image('jump_down', 'Sprites/falling.png');
+  this.load.image('kick', 'Sprites/shooting.png');
+  // this.load.image('dude', 'Sprites/sprite_sheet.png');
   this.load.image('hills', 'Sprites/hills.jpg');
 }
 
 function create() {
   this.add.image(0, 0, 'hills').setOrigin(0, 0);
 
-  var player = this.physics.add.sprite(screen_w / 2, 200, 'dude');
+  player = this.physics.add.sprite(screen_w / 2, 200, 'idle');
   player.setBounce(0.2);
   player.setCollideWorldBounds(true);
   player.setGravityY(300);
 
   this.anims.create({
     key: 'Right',
-    frames: [ { key: 'dude', frame: 4 } ],
-    frameRate: 10,
-    // repeat: -1
+    frames: [{ key: 'idle'}, {key: 'move_right'}, {key: 'idle'}],
+    frameRate: 5,
+    repeat: 0
   });
-  player.anims.play('Right', true);
+
+  this.anims.create({
+    key: 'Left',
+    frames: [{key: 'idle'}, {key: 'move_left'}, {key: 'idle'}],
+    frameRate: 5,
+    repeat: 0
+  });
+
+  this.anims.create({
+    key: 'Up',
+    frames: [{key: 'jump_up'}],
+    frameRate: 20
+  });
+
+  this.anims.create({
+    key: 'Down',
+    frames: [{key: 'jump_down'}],
+    frameRate: 20
+  });
+
+  player.anims.play('Down', true);
+
+  cursors = this.input.keyboard.createCursorKeys();
+
 }
 
+function update() {
+  if (cursors.left.isDown) {
+    player.setVelocityX(-160);
+    player.anims.play('Left', true);
+  } else if (cursors.right.isDown) {
+    player.setVelocityX(160);
+    player.anims.play('Right', true);
+  }
 
+  if (cursors.up.isDown && player.body.position.y >= 602) {
+    player.setVelocityY(-330);
+  }
+}
 
 
 
